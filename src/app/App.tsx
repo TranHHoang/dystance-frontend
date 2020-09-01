fe93ea69c70bcb33f03bc76f53fd102ba8f8d706
TranHoang <hoangthhe130065@fpt.edu.vn>
2020-09-01 20:34:39 +0700

    DL-49: Basic login and register form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { hot } from "react-hot-loader";
-import { PostList } from "../example/post/PostList";
-import { AddPostForm } from "../example/post/AddPostForm";
+import { SignInForm } from "../components/account-management/sign-in/SignInForm";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { RegisterForm } from "../components/account-management/register/RegisterForm";
 
 export default hot(module)(function App() {
     return (
-        <React.Fragment>
-            <AddPostForm />
-            <PostList />
-        </React.Fragment>
+        <BrowserRouter>
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    render={() => (
+                        <React.Fragment>
+                            <SignInForm />
+                            {/*<AddPostForm />*/}
+                            {/*<PostList />*/}
+                        </React.Fragment>
+                    )}
+                />
+                <Route exact path="/register" component={RegisterForm} />
+                <Redirect to="/" />
+            </Switch>
+        </BrowserRouter>
     );
 });
