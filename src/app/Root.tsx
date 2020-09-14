876fd8b818533399f1374b46093172c760369804
minhho3199 <cryaosis132@gmail.com>
2020-09-14 08:20:33 +0700

    Fix bug where background image is on the homepage

--- src/app/Root.tsx
+++ src/app/Root.tsx
@@ -15,14 +15,10 @@ const theme = {
   }
 };
 const Container = styled.div`
- /* background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('https://images.wallpaperscraft.com/image/minimalism_sky_clouds_sun_mountains_lake_landscape_95458_1920x1080.jpg'); */
-  background-image: url("https://images.wallpaperscraft.com/image/minimalism_sky_clouds_sun_mountains_lake_landscape_95458_1920x1080.jpg");
-  background-size: cover;
-  /* background-color: ${(props) => props.theme.rainbow.palette.background.secondary}; */
+  /* background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('https://images.wallpaperscraft.com/image/minimalism_sky_clouds_sun_mountains_lake_landscape_95458_1920x1080.jpg'); */
+
+  background-color: ${(props) => props.theme.rainbow.palette.background.secondary};
   height: 100%;
-  overflow: auto;
-  display: flex;
-  align-items: center;
 `;
 ReactDOM.render(
   <Provider store={store}>
